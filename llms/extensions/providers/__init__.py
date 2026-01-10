856033e9bb449fc3f7f5997f59c7ea8482fb1f20
Demis Bellot <demis.bellot@gmail.com>
2026-01-11 03:18:39 +0800

    Add support for cerebras

--- llms/extensions/providers/__init__.py
+++ llms/extensions/providers/__init__.py
@@ -1,4 +1,5 @@
 from .anthropic import install_anthropic
+from .cerebras import install_cerebras
 from .chutes import install_chutes
 from .google import install_google
 from .nvidia import install_nvidia
@@ -8,11 +9,12 @@ from .openrouter import install_openrouter
 
 def install(ctx):
     install_anthropic(ctx)
+    install_cerebras(ctx)
     install_chutes(ctx)
     install_google(ctx)
+    install_nvidia(ctx)
     install_openai(ctx)
     install_openrouter(ctx)
-    install_nvidia(ctx)
 
 
 __install__ = install
