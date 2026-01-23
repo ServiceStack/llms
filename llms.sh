a1d53a13bad9666e6266af1d52c995cc60d8800b
Demis Bellot <demis.bellot@gmail.com>
2026-01-23 10:46:49 +0800

    handle different python environments

--- llms.sh
+++ llms.sh
@@ -4,4 +4,19 @@ cp ./llms/providers-extra.json ~/.llms/providers-extra.json
 cp ./llms/providers.json ~/.llms/providers.json
 cp ./llms/llms.json ~/.llms/llms.json
 
-python -m llms "$@"
+if [ -d .venv ]; then
+    # if not activated, activate
+    if [ "$(basename "$VIRTUAL_ENV")" != "venv" ]; then
+        source .venv/bin/activate
+    fi
+    if command -v uv &> /dev/null; then
+        uv run -m llms "$@"
+    fi
+elif command -v python3 &> /dev/null; then
+    python3 -m llms "$@"
+elif command -v python &> /dev/null; then
+    python -m llms "$@"
+else
+    echo "python or uv not found"
+    exit 1
+fi
