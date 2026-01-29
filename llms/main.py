5dbd9ac14f5578bf1c3a898e1ec2001c4f8d421b
Demis Bellot <demis.bellot@gmail.com>
2026-01-30 01:22:51 +0800

    Bump version to 3.0.22

--- llms/main.py
+++ llms/main.py
@@ -57,7 +57,7 @@ try:
 except ImportError:
     HAS_PIL = False
 
-VERSION = "3.0.21"
+VERSION = "3.0.22"
 _ROOT = None
 DEBUG = os.getenv("DEBUG") == "1"
 MOCK = os.getenv("MOCK") == "1"
--- llms/ui/ai.mjs
+++ llms/ui/ai.mjs
@@ -6,7 +6,7 @@ const headers = { 'Accept': 'application/json' }
 const prefsKey = 'llms.prefs'
 
 export const o = {
-    version: '3.0.21',
+    version: '3.0.22',
     base,
     prefsKey,
     welcome: 'Welcome to llms.py',
