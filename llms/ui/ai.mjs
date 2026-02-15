8202079b9524bbe2b3dfbbc84a65c656ef57f58f
Demis Bellot <demis.bellot@gmail.com>
2026-02-15 18:02:03 +0800

    Bump version to 3.0.33

--- llms/ui/ai.mjs
+++ llms/ui/ai.mjs
@@ -6,7 +6,7 @@ const headers = { 'Accept': 'application/json' }
 const prefsKey = 'llms.prefs'
 
 export const o = {
-    version: '3.0.32',
+    version: '3.0.33',
     base,
     prefsKey,
     welcome: 'Welcome to llms.py',
