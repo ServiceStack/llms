5f31291f3ddb43313070da467700ab8dcd95e43a
Demis Bellot <demis.bellot@gmail.com>
2026-06-12 18:54:34 +0800

    fixes

--- llms/extensions/providers/openrouter.py
+++ llms/extensions/providers/openrouter.py
@@ -86,5 +86,17 @@ def install_openrouter(ctx):
             super().__init__(**kwargs)
             self.modalities["image"] = OpenRouterGenerator(**kwargs)
 
+        async def process_chat(self, chat, provider_id=None):
+            ret = await super().process_chat(chat, provider_id)
+            chat.pop("modalities", None)
+            chat.pop("enable_thinking", None)
+            messages = chat.get("messages", []).copy()
+            for message in messages:
+                message.pop("timestamp", None)
+                message.pop("reasoning", None)
+                message.pop("refusal", None)
+            ret["messages"] = messages
+            return ret
+
     ctx.add_provider(OpenRouterGenerator)
     ctx.add_provider(OpenRouterProvider)
